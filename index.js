3ab29296794a44269cd3cf44c7c216f9c7253d8e
Sumer   Gaikwad <sg.comparch@gmail.com>
2020-11-26 06:18:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,23 +1,29 @@
 function findMinAndRemove(array){
-  let min = array[0];
-  let minIndex = 0;
-  for (let i = 0; i < array.length; i++) {
-    let currentElement = array[i];
-    if (array[i] < min) {
-      min = array[i];
-      minIndex = i;
+  var min = array[0]
+  var minIndex = 0
+  for (var i = 1; i < array.length; i++)
+  {
+    if (min > array[i])
+    {
+      min = array[i]
+      minIndex = i
     }
   }
-  array.splice(minIndex, 1);
-  return min;
+  array.splice(minIndex, 1)
+  return min
 }
 
 function selectionSort(array){
-  let newMin;
-  let sorted = [];
-  while (array.length != 0) {
-    newMin = minAndRemove(array);
-    sorted.push(newMin);
+  
+  let sorted[]
+  let newMin
+  
+  while (array.length != 0)
+  {
+    console.log(array)
+    newMin = findMinAndRemove(array)
+    sorted.push(newMin)
   }
-  return sorted;
+  
+  return sorted
 }
