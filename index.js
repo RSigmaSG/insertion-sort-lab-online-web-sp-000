97214233d267f824804abd49871a24d92aff51d8
Sumer   Gaikwad <sg.comparch@gmail.com>
2020-11-26 06:09:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 function findMinAndRemove(array){
-
+  var min = array[0]
+  for (var i = 1; i < array.length; i++)
+  {
+    if (min > array[i])
+    {
+      min = array[i]
+    }
+  }
+  return min
 }
 
 function selectionSort(array){
+  
+  var sorted
+  
+  for (var i = 0; array.length !== 0 ; i++)
+  {
+    
+  }
 
 }
